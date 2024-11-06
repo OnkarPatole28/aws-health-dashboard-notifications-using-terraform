abeb5dc9edadfa93b0b255961f55c0ba33a89cd2
onkarpatole <onkarpatole28@gmail.com>
2024-11-06 10:41:38 -0500

    Added IAM Role and Policy For Eventbridge To Send Notifications to SNS Topic.

--- main.tf
+++ main.tf
@@ -34,4 +34,45 @@ resource "aws_cloudwatch_event_target" "sns_topic" {
   target_id = "sendToSNS"
   arn       = aws_sns_topic.aws_health_dashboard_scheduled_changes.arn
 }
+# Step 5: IAM Role for EventBridge to send messages to SNS
+resource "aws_iam_role" "eventbridge_role" {
+  name = "iam-role-for-eventbridge-to-sns"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Principal = {
+        Service = "events.amazonaws.com"
+      }
+      Effect = "Allow"
+      Sid    = ""
+    }]
+  })
+}
+
+# Step 6: Create a custom policy for EventBridge to publish messages to SNS
+resource "aws_iam_policy" "eventbridge_sns_publish_policy" {
+  name        = "eventbridge-sns-publish-policy"
+  description = "Policy to allow EventBridge to send notifcation to SNS"
+  
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "sns:Publish",
+        Resource = aws_sns_topic.aws_health_dashboard_scheduled_changes.arn
+      }
+    ]
+  })
+}
+
+# Step 7: Attach the custom policy to the IAM Role
+resource "aws_iam_policy_attachment" "eventbridge_policy_attachment" {
+  name       = "eventbridge-to-sns-policy-attachment"
+  roles      = [aws_iam_role.eventbridge_role.name]
+  policy_arn = aws_iam_policy.eventbridge_sns_publish_policy.arn
+}
+
 
